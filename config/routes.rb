458ec388a63b99b41ae64f41bd399efa107317fe
Andrew Kerr <me+github@andrewjkerr.com>
2016-04-24 11:23:29 -0400

    reorg routes so auth works!

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :markers
   resources :trails
-  resources :users
   post '/users/authenticate', to: 'users#authenticate'
+  resources :users
   get '/trails/:id/markers', to: 'trails#markers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
