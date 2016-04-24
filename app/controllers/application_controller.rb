ea6ef29b3e60a558c514b8e97482b52f41c54b0f
Andrew Kerr <me+github@andrewjkerr.com>
2016-04-24 04:09:51 -0400

    cors ugh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
+  after_filter :cors_headers
+
+  def cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
   # Verify that auth token is valid
   def verify_user
     @user = User.find_by_auth_token(params[:auth_token])
